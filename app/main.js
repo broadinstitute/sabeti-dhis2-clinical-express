5f3daa596e43eefc9d5ea33e907bbc2945e55302
jotasolano <jsolano91@gmail.com>
2017-08-02 11:21:26 -0400

    geoJSON object ready. Attempting to dispatch

--- app/main.js
+++ app/main.js
@@ -1,8 +1,11 @@
 require('isomorphic-fetch');
 import * as axios from 'axios';
 import api from './ClientApi';
+import {dispatch} from 'd3';
 
+const dis = dispatch('loaded', 'error');
 
+function DataLoader(){
 axios.get(`http://localhost:3000/api/dhis`)
 	.then(response => {
 		const data = response.data;
@@ -10,9 +13,7 @@ axios.get(`http://localhost:3000/api/dhis`)
 		let cases = [];
 
 		//extract the dataValues arrays
-		data.events.forEach((event) => {
-			cases.push(event.dataValues);
-		});
+		data.events.forEach(event => { cases.push(event.dataValues); });
 
 		//create array with all dataElements:
 		for (var i = 0; i < cases.length; i++) {
@@ -25,28 +26,31 @@ axios.get(`http://localhost:3000/api/dhis`)
 
 		//get list of unique dataElements: [id1,id2,...]
 		let listOfUniqueDEs = [... new Set(listOfDEs)].join(',');
+
+		//get "dictionary" of dataElementID : displayValue
 		return axios.get(`http://localhost:3000/api/dataElements/${listOfUniqueDEs}`)
 	})
-	.then(response => {
+	.then(response => { //dictionary
 		const data = response.data;
 		let namesMap = {};
 		
-		data.dataElements.forEach(el => {
-		namesMap[el.id] = el.displayName;
-		})
+		data.dataElements.forEach(el => { namesMap[el.id] = el.displayName; })
 		return namesMap;
 	})
-	.then(response => {
+	.then(response => { //names mapping
 		let namesMap = response;
 		axios.get(`http://localhost:3000/api/dhis`)
 			.then(response => {
 				const data = response.data;
 				let cases = [];
+				let dates = [];
 				let values = [];
 
-				data.events.forEach((event) => {
-					cases.push(event.dataValues);
-				});
+				//extract the dataValues arrays
+				data.events.forEach(event => { cases.push(event.dataValues); });
+
+				// extract the eventDate value
+				data.events.forEach(event => { dates.push(event.eventDate); });
 				
 				// create nested array with dataElement:value objects
 				for (var i = 0; i < cases.length; i++) {
@@ -58,7 +62,13 @@ axios.get(`http://localhost:3000/api/dhis`)
 					}
 					values.push(object);
 				}
-				console.log(values);
+
+				//add the eventDate property to values
+				for (const [index, object] of values.entries()){
+					values[index].eventDate = parseTime(dates[index]);
+				}
+
+				//create a geoJSON-like object
 				let geoJSON = [];
 				for (const [index, object] of values.entries()){
 					geoJSON[index] = {
@@ -73,15 +83,40 @@ axios.get(`http://localhost:3000/api/dhis`)
 			      ]
 			    } //END geoJSON
 				}
+
+				//dispatch geoJSON to main site
+				function exports(){
+				dis.call('loaded', null, {dummyCases: geoJSON});
+				}
+
+				exports.on = function(event, callback){
+					dis.on(event, callback);
+					return this;
+				}
+
+				return exports;
 				console.log(geoJSON);
 			})
 	})
+}
 
-	function getCoords(str){
-		if (str === undefined){
-			return [undefined, undefined]
-		} else {
-			let coords = str.replace(/\[/,'').replace(/\]/,'').split(',')
-			return [+coords[0], +coords[1]]
-		}
+function getCoords(str){
+	if (str === undefined){
+		return [undefined, undefined]
+	} else {
+		let coords = str.replace(/\[/,'').replace(/\]/,'').split(',')
+		return [+coords[0], +coords[1]]
 	}
+}
+
+function parseTime(str){
+	var date = str.split('T')[0].split('-');
+	var year = +date[0];
+	var month = +date[1]-1;
+	var day = +date[2];
+
+	return new Date (year, month, day);
+}
+
+
+export default DataLoader;
