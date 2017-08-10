4822a4be84584580e56e473876e4ac45047b3194
jotasolano <jsolano91@gmail.com>
2017-08-10 10:34:05 -0400

    optimized button toggles and update and brushing events

--- app/main.js
+++ app/main.js
@@ -17,9 +17,12 @@ import Fasta from './Fasta';
 import Secret from '../Secret';
 
 
-let dis = d3.dispatch('timeUpdate', 'hexHover');
+let dis = d3.dispatch('timeUpdate', 'buttonTrig');
 let details = Details();
 
+let dateMap = d3.map()
+  .set("startDate", '')
+  .set('endDate', '');
 
 // ** ------- MODULES INIT ------- **
 let timeline = Timeline().on('disBrush', data => {
@@ -27,102 +30,70 @@ let timeline = Timeline().on('disBrush', data => {
       endDate = data.endDate;
 
   dis.call('timeUpdate', null, {start: startDate, end: endDate});
-
   d3.select('#details').datum({start: startDate, end: endDate}).call(details);
-
 });
 
 
-
-function redraw(array) {
+function redraw(array, _points, _hexes) {
+  let coords = [];
   let filtered = [];
+  filtered = _.filter(array, function(el) { return el.features[0].properties.eventDate >= dateMap.get("startDate") && el.features[0].properties.eventDate <= dateMap.get("endDate") })
 
-  dis.on('timeUpdate', d => {
-    let coords = [];
-    filtered = _.filter(array, function(el) {
-      return el.features[0].properties.eventDate >= d.start && el.features[0].properties.eventDate <= d.end
+
+  function projectPoint(x, y) {
+    let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+  }
+
+  function updateHexCoords(array) {
+    let newArr = []
+    array.forEach(el => {
+      let point = map.latLngToLayerPoint([el[1], el[0]]);
+      newArr.push([point.x, point.y, el[2]]);
+    });
+    return newArr;
+  }
+
+
+  if (_points) {
+    dis.on('timeUpdate.test buttonTrig', d => {
+      if (document.getElementById('cases-btn').getAttribute('value') == 1){ //check if cases button is on!
+        let filtered = [];
+        filtered = _.filter(array, function(el) { return el.features[0].properties.eventDate >= dateMap.get("startDate") && el.features[0].properties.eventDate <= dateMap.get("endDate") })
+        drawPoints(filtered)
+      }
     })
 
-    //array with all the coordinates (used for hexagonal binning)    
-    filtered.forEach(el => {coords.push([el.features[0].geometry.coordinates[0], el.features[0].geometry.coordinates[1], el.features[0].properties['GenBank IDs']]) });
-
-    drawFeatures(filtered);
-
-    function projectPoint(x, y) {
-      let point = map.latLngToLayerPoint(new L.LatLng(y, x));
-      this.stream.point(point.x, point.y);
-    }
-
-    function updateHexCoords(array) {
-      let newArr = []
-      array.forEach(el => {
-        let point = map.latLngToLayerPoint([el[1], el[0]]);
-        newArr.push([point.x, point.y, el[2]]);
-      });
-      return newArr;
-    }
+  } else if (_points && _hexes) {
+    drawPoints(filtered)
+    drawHexes(array)
+  } else drawHexes(array);
 
 
-    function drawFeatures(data) {
-      const svg = d3.select('#map').select('svg');
-      const width = +svg.attr('width');
-      const height = +svg.attr('height');
-      
+  function drawPoints(data){
+    console.log('drawPoints triggered')    
+    const svg = d3.select('#map').select('svg');
+    let transform = d3.geoTransform({point: projectPoint});
+    let path = d3.geoPath().projection(transform);
+    path.pointRadius(7);
 
-      let transform = d3.geoTransform({point: projectPoint});
-      let path = d3.geoPath().projection(transform);
-      path.pointRadius(7);
+    map.on('zoom movend viewreset', update);
+    update();
 
-      let hex = hexbin()
-        .radius(30)
-        .extent([[0, 0], [width, height]])
-
-      let color = d3.scaleQuantize()
-        .domain([1, 7])
-        .range(['#fef0d9','#fdcc8a','#fc8d59','#d7301f'])
-
-      map.on('zoom movend viewreset', update);
-      update();
-
-
-      // document.getElementById('cases-btn').onclick = showCases;
-      // document.getElementById('cluster-btn').onclick = showCluster;
-
-      // function showCases() {
-      //   let points = d3.selectAll('.point-case');
-      //   let hexagons = d3.selectAll('.hexagon');
-
-      //   points.classed('hide', !points.classed("hide"));
-      //   hexagons.classed('hide', !hexagons.classed("hide"));
-      // }
-
-      // function showCluster() {
-      //   let points = d3.selectAll('.point-case');
-      //   let hexagons = d3.selectAll('.hexagon');
-
-      //   points.classed('hide', !points.classed("hide"));
-      //   hexagons.classed('hide', !hexagons.classed("hide"));
-      // }
-
-      function update() {
-
-        let featureElement = svg.selectAll('.point-case')
-          .data(data)
-
-
-        featureElement.exit().remove();
-
+    function update(){
+      if (document.getElementById('cases-btn').getAttribute('value') == 1){
+        let featureElement = svg.selectAll('.point-case').data(data)
+        
+        featureElement.exit().remove(); 
         featureElement
-          .enter()
-          .append('path')
+          .enter().append('path')
+          .attr('d', path)
+          .attr('fill', 'red')
           .attr('class', 'point-case')
           .merge(featureElement)
           .attr('d', path)
-          .attr('stroke', 'gray')
-          .attr('fill', 'red')
           .attr("fill-opacity", 0.4)
           .attr('style', 'pointer-events:visiblePainted;')
-          .classed('hide', false)
           .on('mouseover', function(d) {
             console.log(d);
             let id = d.features[0].properties['GenBank IDs'];
@@ -131,68 +102,143 @@ function redraw(array) {
           .on('mouseout', function(d) {
             let detailsNode = document.getElementById('hexDetails');
             detailsNode.innerHTML = '';
-          })
-
-        d3.selectAll('.hexagon').remove();
-
-        let hexagons = svg
-          .selectAll('.hexagon')
-          .data(hex(updateHexCoords(coords)).sort(function(a,b) { return b-length - a.length; }));
-
-        hexagons.exit().remove();
-
-        //ENTER+UPDATE
-        hexagons.enter().append('path')
-          
-          .attr('fill-opacity', .5)
-            .attr('class', 'hexagon')
-            .merge(hexagons)
-            .attr("d", hex.hexagon())
-            .attr("fill", function(d) { return color(d.length); })
-            .attr('stroke', 'gray')
-            .attr('style', 'pointer-events:visiblePainted;')
-            .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; })
-            
+          });
+      }
+    }//end of Update
+  }
 
 
 
-            .on('mouseover', function(d) {
-              let listOfIds = [];
-              let detailsNode = document.getElementById('hexDetails');
-              let coords = map.layerPointToLatLng([d.x, d.y]);
+  function drawHexes(data){
+    console.log('drawHexes triggered')
+  }
 
-              d3.select(this).classed('hexHover', true);
 
-              d.forEach(d => {
-                listOfIds.push(d[2])
-                return listOfIds;
-              })
+  // dis.on('timeUpdate', d => {
+  //   let filtered = [];
+  //   let coords = [];
+  //   filtered = _.filter(array, function(el) {
+  //     return el.features[0].properties.eventDate >= d.start && el.features[0].properties.eventDate <= d.end
+  //   })
+
+  //   //array with all the coordinates (used for hexagonal binning)    
+  //   filtered.forEach(el => {coords.push([el.features[0].geometry.coordinates[0], el.features[0].geometry.coordinates[1], el.features[0].properties['GenBank IDs']]) });
+
+
+
+  //   drawFeatures(filtered);
+
+
+  //   function drawFeatures(data) {
+  //     const svg = d3.select('#map').select('svg');
+  //     const width = +svg.attr('width');
+  //     const height = +svg.attr('height');
+
+  //     let transform = d3.geoTransform({point: projectPoint});
+  //     let path = d3.geoPath().projection(transform);
+  //     path.pointRadius(7);
+
+  //     let hex = hexbin()
+  //       .radius(30)
+  //       .extent([[0, 0], [width, height]])
+
+  //     let color = d3.scaleQuantize()
+  //       .domain([1, 7])
+  //       .range(['#fef0d9','#fdcc8a','#fc8d59','#d7301f'])
+
+  //     let featureElement = svg.selectAll('.point-case')
+  //         .data(data)
+  //         .enter()
+  //         .append('path')
+  //         .attr('fill', 'purple')
+
+  //     map.on('zoom movend viewreset', update);
+  //     update();
+
+  //     function update() {
+  //       // let featureElement = svg.selectAll('.point-case')
+  //       //   .data(data)
+
+  //       featureElement.exit().remove();
+
+  //       featureElement
+  //         .attr('class', 'point-case')
+  //         .merge(featureElement)
+  //         .attr('d', path)
+  //         .attr('stroke', 'gray')
+  //         // .attr('fill', 'red')`
+  //         .attr("fill-opacity", 0.4)
+  //         .attr('style', 'pointer-events:visiblePainted;')
+  //         .on('mouseover', function(d) {
+  //           console.log(d);
+  //           let id = d.features[0].properties['GenBank IDs'];
+  //           Fasta(id);
+  //         })
+  //         .on('mouseout', function(d) {
+  //           let detailsNode = document.getElementById('hexDetails');
+  //           detailsNode.innerHTML = '';
+  //         })
+
+  //       d3.selectAll('.hexagon').remove();
+
+  //       let hexagons = svg
+  //         .selectAll('.hexagon')
+  //         .data(hex(updateHexCoords(coords)).sort(function(a,b) { return b-length - a.length; }));
+
+  //       hexagons.exit().remove();
+
+  //       //ENTER+UPDATE
+  //       hexagons.enter().append('path')
+  //         .attr('fill-opacity', .5)
+  //           .attr('class', 'hexagon')
+  //           .merge(hexagons)
+  //           .attr("d", hex.hexagon())
+  //           .classed('hide', false)
+  //           .attr("fill", function(d) { return color(d.length); })
+  //           .attr('stroke', 'gray')
+  //           .attr('style', 'pointer-events:visiblePainted;')
+  //           .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; })
+
+  //           .on('mouseover', function(d) {
+  //             let listOfIds = [];
+  //             let detailsNode = document.getElementById('hexDetails');
+  //             let coords = map.layerPointToLatLng([d.x, d.y]);
+
+  //             d3.select(this).classed('hexHover', true);
+
+  //             d.forEach(d => {
+  //               listOfIds.push(d[2])
+  //               return listOfIds;
+  //             })
               
-              const markup = `
-              <h5>
-                ${d.length} cases near</span>
-              </h5>
-              <h5>
-                ${coords.lat.toFixed(2)}, ${coords.lng.toFixed(2)}
-              </h5>
-              <ul></ul>
-              `;
-              detailsNode.innerHTML = markup;
-            })
+  //             const markup = `
+  //             <h5>
+  //               ${d.length} cases near</span>
+  //             </h5>
+  //             <h5>
+  //               ${coords.lat.toFixed(2)}, ${coords.lng.toFixed(2)}
+  //             </h5>
+  //             <ul></ul>
+  //             `;
+  //             detailsNode.innerHTML = markup;
+  //           })
 
-            .on("mouseout", function(d) {
-              d3.select(this).classed('hexHover', !d3.select(this).classed('hexHover'));
+  //           .on("mouseout", function(d) {
+  //             d3.select(this).classed('hexHover', !d3.select(this).classed('hexHover'));
 
-              let detailsNode = document.getElementById('hexDetails');
-              detailsNode.innerHTML = '';
-            });
-      }//-->END .update()
-    } //-->END .drawFeatures()
+  //             let detailsNode = document.getElementById('hexDetails');
+  //             detailsNode.innerHTML = '';
+  //           });
+  //     }//-->END .update()
+  //   } //-->END .drawFeatures()
 
 
-  });//-->END timeUpdate()
+  // });//-->END timeUpdate()
 }//-->END redraw()
 
+dis.on('timeUpdate.mapping', d => {
+  dateMap.set("startDate", d.start).set("endDate", d.end);
+});
 
 
 // ** ------- DataLoader() ------- **
@@ -200,8 +246,6 @@ let getData = DataLoader()
  .on('loaded', data => {
  		// console.log(data.data)
  		let dummyCases = data.data;
-
-    console.log(dummyCases);
 
     const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
     map = L.map('map').setView([8.4506145, -11.3474766], 9);
@@ -219,45 +263,42 @@ let getData = DataLoader()
     // main(dummyCases);
     d3.select('#timeline').datum(casesByDate).call(timeline);
 
-    redraw(dummyCases); //where all the magic happens
 
-    // function projectPoint(x, y) {
-    //     var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-    //     this.stream.point(point.x, point.y);
-    // }
-    
-    // function drawFeatures(data) {
-    //     var svg = d3.select("#map").select("svg");
-        
-    //     var transform = d3.geoTransform({point: projectPoint});
-    //     var path = d3.geoPath().projection(transform);
+    document.getElementById('cluster-btn').onclick = showCluster;
+    document.getElementById('cases-btn').onclick = showCases;
 
-    //     path.pointRadius(7);
-    
-    //     var featureElement = svg.selectAll("path")
-    //         .data(data)
-    //         .enter()
-    //         .append("path")
-    //         .attr("stroke", "gray")
-    //         .attr("fill", "red")
-    //         .attr("fill-opacity", 1)
-    //         .attr('style', 'pointer-events:visiblePainted;')
-    //         .on('click', function(d) { console.log(d.features[0].properties); });
-    
-    //     map.on("moveend", update);
-    
-    //     update();
-    
+    redraw(dummyCases, true, false); //draw on 
 
-    //     function update() {   
-    //         featureElement.attr("d", path);
-    //     } 
-    
-    // }
+    function showCluster(){
+      d3.selectAll(`path.hexagon`)
+        .classed("hide", function (d, i) {
+          return !d3.select(this).classed("hide");
+        });
+    }
+
+    function showCases(){
+      let filtered = [];
+      let button = this;
+      let activeState = button.getAttribute('value');
+      
+      if (activeState == 0) {
+        button.setAttribute('value', 1); //if button is active
+        console.log('cases turned to 1')
+        dis.call('buttonTrig', null, {})
+
+      redraw(dummyCases, true, false);
+
+      } else { // if button is disabled
+        button.setAttribute('value', 0)
+        d3.selectAll('path.point-case').remove();
+      }
+    }
 
 
- 
+
   }); //-->END .on('loaded')
+
+
 
 
   // ** ------- DATA QUERY ------- **
